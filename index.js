b60921d1ff108822754f63dae1e92b3fa4ae1b09
malual <malual@DESKTOP-8LEI2ON.localdomain>
2023-10-09 19:25:56 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let divideSum=2000/100
+    return divideSum
+}
+const square=(number)=>{
+  
+    return number*number
+}
+const result=square(5)
+
+const add=(num1,num2)=>{
+    return num1+num2
+}
+const answer=add(5,6)
